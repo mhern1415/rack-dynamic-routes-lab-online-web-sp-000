d0b2f288a967e6574e7806e3fbe8ea5b72e821fb
Mark Hernandez <m.hern1415@gmail.com>
2019-09-20 18:43:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       item = @@items.find{|i| i.name == item_name}
 
       if item.nil?
-        resp.write "No item found"
+        resp.write "Item not found"
         resp.status = 400
       else
         resp.write item.price
