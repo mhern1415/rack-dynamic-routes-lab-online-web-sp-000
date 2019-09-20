51baacd004f75eddba73b21fa5d3480e2ffd7d9c
Mark Hernandez <m.hern1415@gmail.com>
2019-09-20 18:39:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,25 @@
 class Application
   
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    
+    
+    
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
+    
+    
+    
+  
   
